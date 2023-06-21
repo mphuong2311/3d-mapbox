e879b00f4f051a88e991b101367b183c4d8c1c63
PhuongNM <minhphuongitst@gmail.com>
2023-06-21 21:41:40 +0700

    Success log this.map

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,47 @@
 <template>
-  <div class="map-view">
+  <div class="map-container">
     <MglMap
-      ref="pkamap"
-      :accessToken="accessToken"
-      :mapStyle="mapStyle"
+      class="map"
+      :map-style="mapStyle"
       :center="center"
       :zoom="zoom"
-      :bearing="bearing"
-      :pitch="pitch"
-    />
+      :attribution-control="false"
+      @load="onMapLoad"
+    >
+    </MglMap>
   </div>
 </template>
 
 <script>
-import { MAPBOX_ACCESS_TOKEN, MAPBOX_STYLE } from "@/config/mapbox";
-import { MglMap } from "vue-mapbox";
-
+import { MAPBOX_STYLE } from "@/config/mapbox";
+import customLayer from "./customLayer";
 export default {
   name: "HelloWorld",
-  components: {
-    MglMap,
-  },
+  components: {},
   data() {
     return {
-      accessToken: MAPBOX_ACCESS_TOKEN,
       mapStyle: MAPBOX_STYLE,
       center: [105.74764, 20.9617],
-      zoom: 17,
-      bearing: 60,
+      zoom: 18,
+      bearing: 0,
       pitch: 60,
     };
+  },
+  methods: {
+    onMapLoad({ map }) {
+      this.map = map;
+      console.log(this.map);
+      map.on("style.load", () => {
+        map.addLayer(customLayer, "waterway-label");
+      });
+    },
   },
 };
 </script>
 
-<style lang="css">
-.map-view {
+<style>
+.map-container {
+  width: 100%;
   height: 90vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 // Mapbox CSS
 import "mapbox-gl/dist/mapbox-gl.css";
-Vue.config.productionTip = false
+import VueMgl, { mgl } from "vue-mgl";
+import { MAPBOX_ACCESS_TOKEN } from "@/config/mapbox";
+Vue.use(VueMgl);
+mgl.accessToken = MAPBOX_ACCESS_TOKEN;
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
