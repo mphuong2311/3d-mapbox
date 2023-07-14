30702e382a779a1137c77d043898a6c773ba186e
LongNH <nguyenhailongtlu@gmail.com>
2023-07-14 10:11:28 +0700

    bo cua tesla

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,10 +4,10 @@
 
 <script>
 import * as THREE from "three";
-import * as Stats from "stats.js";
+import ROSLIB from "roslib";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import Xbus from "@/assets/xbus/Xbus2.gltf";
-import objList from "./object";
+
 export default {
   name: "RoadComponent",
   data() {
@@ -15,20 +15,17 @@ export default {
       scene: null,
       camera: null,
       renderer: null,
-      xbus: null,
-      road: null,
-      object: null,
       objects: [],
-      lineLeft: null,
-      lineRight: null,
-      lineCenter: null,
+      messageFromROS: null,
+      road: null,
     };
   },
   mounted() {
+    this.initThree();
     this.load3DModel();
-    this.initThree(objList);
+    this.initROS();
+    this.animate();
   },
-
   methods: {
     load3DModel() {
       const loader = new GLTFLoader();
@@ -37,8 +34,8 @@ export default {
         this.xbus = gltf.scene;
 
         // Đặt vị trí và kích thước của mô hình
-        this.xbus.position.set(5, 0, 50); // Đặt mô hình ở trên mặt đường
-        this.xbus.scale.set(3.5, 3.5, 3.5); // Đặt tỷ lệ kích thước của mô hình
+        this.xbus.position.set(0, 0, 0); // Đặt mô hình ở trên mặt đường
+        this.xbus.scale.set(1, 1, 1); // Đặt tỷ lệ kích thước của mô hình
 
         // Tạo vật liệu MeshPhongMaterial cho mô hình
         var xbusMaterial = new THREE.MeshPhongMaterial({
@@ -56,10 +53,7 @@ export default {
         this.scene.add(this.xbus);
       });
     },
-    initThree(objList) {
-      this.stats = new Stats();
-      this.stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-      this.$refs.mount.appendChild(this.stats.dom);
+    initThree() {
       // Khởi tạo scene, camera và renderer
       this.scene = new THREE.Scene();
       this.camera = new THREE.PerspectiveCamera(75, 500 / 500, 0.1, 1000);
@@ -69,11 +63,10 @@ export default {
       this.$refs.mount.appendChild(this.renderer.domElement);
 
       // Đặt vị trí camera
-      this.camera.position.set(0, 35, 80); // Đặt camera ở trên cao nhìn xuống
+      this.camera.position.set(0, 20, 20); // Đặt camera ở trên cao nhìn xuống
       this.camera.lookAt(0, 0, 0); // Hướng nhìn của camera vào tâm của scene
-
       // Tạo đường
-      var roadWidth = 11; // Chiều rộng của mỗi làn đường
+      var roadWidth = 2; // Chiều rộng của mỗi làn đường
       var pointsLeft = [];
       var pointsRight = [];
       var pointsCenter = [];
@@ -83,50 +76,6 @@ export default {
         pointsCenter.push(new THREE.Vector3(0, 0, i * 10 - 505));
         pointsCenter.push(new THREE.Vector3(0, 0, i * 10 - 495));
       }
-
-      // // Tạo hình hộp để mô phỏng ô tô
-      // var xbusGeometry = new THREE.BoxGeometry(6, 12, 12); // Chiều dài, chiều rộng, chiều cao
-      // var xbusMaterial = new THREE.MeshBasicMaterial({ color: "pink" });
-      // this.xbus = new THREE.Mesh(xbusGeometry, xbusMaterial);
-      // // Đặt vị trí của ô tô
-      // this.xbus.position.set(0, 0, 0); // Đặt ô tô ở trên mặt đường
-      //tạo obj dựa vào objList
-      objList.forEach((obj) => {
-        const key = Object.keys(obj)[0];
-        const position = obj[key].position;
-        const dimensions = obj[key].dimensions;
-        const speed = obj[key].speed; // Lấy giá trị speed từ objList
-
-        var objectGeometry = new THREE.BoxGeometry(
-          dimensions.x,
-          dimensions.y,
-          dimensions.z
-        );
-        var objectMaterial = new THREE.MeshBasicMaterial({ color: "green" });
-        var object = new THREE.Mesh(objectGeometry, objectMaterial);
-        object.position.set(position.x, position.y, position.z);
-
-        this.scene.add(object);
-
-        // Lưu trữ đối tượng và speed trong objects
-        this.objects[key] = {
-          object: object,
-          speed: speed,
-        };
-      });
-      // // Tạo object
-      // var objectGeometry = new THREE.BoxGeometry(6, 12, 12); // Chiều dài, chiều rộng, chiều cao
-      // var objectMaterial = new THREE.MeshBasicMaterial({ color: "green" });
-      // this.object = new THREE.Mesh(objectGeometry, objectMaterial);
-      // this.object.position.set(16, 0, -35); // Đặt ô tô ở trên mặt đường
-
-      // // Tạo person
-      // var personGeometry = new THREE.BoxGeometry(2, 5, 5); // Chiều dài, chiều rộng, chiều cao
-      // var personMaterial = new THREE.MeshBasicMaterial({ color: "green" });
-      // this.person = new THREE.Mesh(personGeometry, personMaterial);
-      // this.person.position.set(-10, 0, -35); // Đặt ô tô ở trên mặt đường
-      // this.scene.add(this.object);
-
       var geometryLeft = new THREE.BufferGeometry().setFromPoints(pointsLeft);
       var geometryRight = new THREE.BufferGeometry().setFromPoints(pointsRight);
       var geometryCenter = new THREE.BufferGeometry().setFromPoints(
@@ -142,6 +91,10 @@ export default {
       this.lineRight = new THREE.Line(geometryRight, material);
       this.lineCenter = new THREE.LineSegments(geometryCenter, dashedMaterial);
       this.lineCenter.computeLineDistances();
+      // Thêm đường vào scene
+      this.scene.add(this.lineLeft);
+      this.scene.add(this.lineRight);
+      this.scene.add(this.lineCenter);
       // Thêm ánh sáng ambient
       var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
       this.scene.add(ambientLight);
@@ -150,30 +103,93 @@ export default {
       var pointLight = new THREE.PointLight(0xffffff, 1);
       pointLight.position.set(0, 50, 50);
       this.scene.add(pointLight);
-      // Thêm đường vào scene
-      this.scene.add(this.lineLeft);
-      this.scene.add(this.lineRight);
-      this.scene.add(this.lineCenter);
-      // Thêm ô tô vào scene
-      this.scene.add(this.xbus);
 
       // Vẽ scene
       this.renderer.render(this.scene, this.camera);
       this.animate();
     },
-    animate() {
-      this.stats.begin();
-      Object.keys(this.objects).forEach((key) => {
-        const object = this.objects[key];
+    createObjectBox(msg) {
+      // Remove existing objects from the scene
+      this.objects.forEach((object) => {
+        this.scene.remove(object);
+      });
+      // Vẽ vật thể
+      this.objects = [];
+      msg.objects.forEach((obj) => {
+        const position = obj.position.pose;
+        const dimensions = obj.shape.dimensions;
+        var geometry = new THREE.BoxGeometry(
+          -dimensions.y,
+          dimensions.z,
+          -dimensions.x
+        );
+        var material = new THREE.MeshBasicMaterial({ color: "green" });
+        var box = new THREE.Mesh(geometry, material);
+        box.position.set(-position.y, position.z, -position.x);
+        box.rotation.set(0, obj.position.angle.roll, 0);
+        this.scene.add(box);
+        this.objects.push(box);
+      });
+      // if (this.road) {
+      //   this.scene.remove(this.road);
+      //   this.road = null;
+      // }
 
-        // Cập nhật vị trí z của từng đối tượng
-        object.object.position.z += object.speed;
+      // const pointsList = msg.map;
+      // const points = [];
+
+      // pointsList.forEach((point, index) => {
+      //   // Kiểm tra điều kiện để chỉ nối các điểm ở giữa
+      //   if (index > 0 && index < pointsList.length - 1) {
+      //     const vector = new THREE.Vector3(-point.y, point.z, -point.x);
+
+      //     // Tạo ma trận quay 45 độ theo trục Z
+      //     const angle = Math.PI / -3; // Góc quay 45 độ (được chuyển từ radian sang độ bằng cách chia cho 180 và nhân cho Math.PI)
+      //     const rotationMatrix = new THREE.Matrix4().makeRotationY(angle);
+
+      //     // Áp dụng ma trận quay vào vector
+      //     vector.applyMatrix4(rotationMatrix);
+
+      //     points.push(vector);
+      //   }
+      // });
+
+      // const geometryPoint = new THREE.BufferGeometry().setFromPoints(points);
+
+      // const material = new THREE.LineBasicMaterial({ color: "gray" });
+
+      // // Tạo đối tượng Line (hoặc LineSegments) với các điểm chỉ nối ở giữa
+      // this.road = new THREE.Line(geometryPoint, material);
+
+      // this.scene.add(this.road);
+    },
+
+    initROS() {
+      const ros = new ROSLIB.Ros({
+        url: "ws://localhost:9090",
       });
 
+      ros.on("connection", () => {
+        console.log("Kết nối tới websocket server đã được mở.");
+      });
+
+      const topic = new ROSLIB.Topic({
+        ros: ros,
+        name: "/show_visualization/visualization",
+        messageType: "autoware_lanelet2_msgs/Visualization",
+        throttle_rate: 0,
+      });
+
+      topic.subscribe((message) => {
+        this.messageFromROS = JSON.parse(JSON.stringify(message));
+        this.createObjectBox(this.messageFromROS);
+      });
+    },
+    animate() {
       // Cập nhật vị trí z của ô tô
-      this.lineLeft.position.z += 0.5;
-      this.lineRight.position.z += 0.5;
-      this.lineCenter.position.z += 0.5;
+      this.lineLeft.position.z += 0.1;
+      this.lineRight.position.z += 0.1;
+      this.lineCenter.position.z += 0.1;
 
       // Kiểm tra vị trí z của lineLeft
       if (this.lineLeft.position.z > 100) {
@@ -189,12 +205,8 @@ export default {
       if (this.lineCenter.position.z > 100) {
         this.lineCenter.position.z = -100;
       }
-
-      // Vẽ lại scene
+      requestAnimationFrame(this.animate.bind(this));
       this.renderer.render(this.scene, this.camera);
-      this.stats.end();
-      // Yêu cầu trình duyệt gọi hàm animate trong frame tiếp theo
-      requestAnimationFrame(this.animate);
     },
   },
 };
@@ -203,6 +215,6 @@ export default {
 <style scoped>
 div {
   width: 100%;
-  height: 50%;
+  height: 100%;
 }
 </style>
